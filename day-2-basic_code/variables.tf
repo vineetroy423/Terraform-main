b706eec1a13e65abf4cc2f24d72c6431b6dec1ee
vineetroy423 <vineetkumaryadav2@gmail.com>
2024-09-06 21:35:25 +0530

    Update variables.tf

--- day-2-basic_code/variables.tf
+++ day-2-basic_code/variables.tf
@@ -1,6 +1,6 @@
 variable "mybucket" {
     type = string
-    description = "update bucket value for my bucket vinnyroy"
+    description = "update bucket value for my bucket vinnyroy yadav"
     default = ""
   
 }
